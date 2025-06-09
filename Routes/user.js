c57b84d0f1c82cf23dfcb891f98a264fa07d7c70
Tek Raj <ranatekraj06@gmail.com>
2025-06-09 12:03:08 +0530

    Create feed api

--- Routes/user.js
+++ Routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const Connection = require("../models/connection");
+const User = require("../models/user");
 
 const userRouter = express.Router();
 
@@ -46,6 +47,38 @@ userRouter.get("/user/connections", userAuth, async(req, res) => {
         })
 
         return res.status(200).json({message: "Retrieve all connections.", data})
+
+    }catch(err){
+        console.error(err)
+        return res.status(500).json({ERROR: err.message})
+    }
+})
+
+userRouter.get("/feed", userAuth, async(req, res) => {
+
+    try{
+        const loggedInUser = req.user;
+
+        const connectionRequest = await Connection.find({
+            $or: [
+                {fromUserId: loggedInUser._id},
+                {toUserId: loggedInUser._id}
+            ]
+        }).select("fromUserId toUserId")
+
+        const hideUserFromFeed = new Set();
+
+        connectionRequest.forEach((req) => {
+            hideUserFromFeed.add(req.fromUserId.toString());
+            hideUserFromFeed.add(req.toUserId.toString())
+        })
+        // console.log(hideUserFromFeed)
+
+        const users = await User.find({
+           $and:[ {_id: {$nin: Array.from(hideUserFromFeed)}}, {_id: {$ne: loggedInUser._id}}]
+        }).select(populate_data)
+
+        return res.status(201).json({message: "Retrieve all feed.", users})
 
     }catch(err){
         console.error(err)
