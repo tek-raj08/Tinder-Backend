5e8672e02aedc3a14b8cef709300db3252d5d427
Tek Raj <ranatekraj06@gmail.com>
2025-06-13 20:53:34 +0530

    update the userAuth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,7 +6,7 @@ const userAuth = async(req, res, next) => {
 
     try{
 
-        const {token} = req.cookies?.token || (req.headers['authorization']?.split(" ")[1])
+        const {token} = req.cookies?.token || req.headers['authorization']
         if(!token){
             return res.status(404).json({message: "Token is not found."})
         }
